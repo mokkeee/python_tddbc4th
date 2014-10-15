48709c9f3fa164d9ed80227a719544b6b035b627
Satoru Motegi <mokky.monkey.14@gmail.com>
2014-10-16 03:32:12 +0900

    @propertyを使用するよう修正

--- acme/person.py
+++ acme/person.py
@@ -3,15 +3,18 @@
 
 class Person:
     def __init__(self, family_name, first_name):
-        self.__family_name = family_name
-        self.__first_name = first_name
+        self._family_name = family_name
+        self._first_name = first_name
 
-    def get_family_name(self):
-        return self.__family_name
+    @property
+    def family_name(self):
+        return self._family_name
 
-    def get_first_name(self):
-        return self.__first_name
+    @property
+    def first_name(self):
+        return self._first_name
 
-    def get_full_name(self):
-        return self.__family_name + self.__first_name
+    @property
+    def full_name(self):
+        return self._family_name + self._first_name
 
--- tests/acme/test_person.py
+++ tests/acme/test_person.py
@@ -6,13 +6,13 @@ from acme.person import Person
 class TestPerson:
     def test_佐藤一郎の名字は佐藤である(self):
         佐藤一郎 = Person('佐藤', '一郎')
-        assert 佐藤一郎.get_family_name() == '佐藤'
+        assert 佐藤一郎.family_name == '佐藤'
 
     def test_佐藤一郎の名前は一郎である(self):
         佐藤一郎 = Person('佐藤', '一郎')
-        assert 佐藤一郎.get_first_name() == '一郎'
+        assert 佐藤一郎.first_name == '一郎'
 
     def test_佐藤一郎の氏名は佐藤一郎である(self):
         佐藤一郎 = Person('佐藤', '一郎')
-        assert 佐藤一郎.get_full_name() == '佐藤一郎'
+        assert 佐藤一郎.full_name == '佐藤一郎'
 
