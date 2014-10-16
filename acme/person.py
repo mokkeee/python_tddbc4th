b606c36903af9875afc208f2d1f550df652aeba3
Satoru Motegi <mokky.monkey.14@gmail.com>
2014-10-16 23:33:05 +0900

    性別を追加

--- acme/person.py
+++ acme/person.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+MALE = 1
+FEMALE = 2
+GENDER = (MALE, FEMALE)
+
 class Person:
-    def __init__(self, family_name, first_name):
-        if family_name is None or family_name.strip() == "":
+    def __init__(self, family_name, first_name, gender = MALE):
+        if not family_name or family_name.strip() == "":
             raise RuntimeError("family_name must required.")
-        elif first_name is None or first_name.strip() == "":
+        elif not first_name or first_name.strip() == "":
             raise RuntimeError("first_name must required.")
+        elif not gender or gender not in GENDER:
+            raise RuntimeError("illegal gender.")
 
         self._family_name = family_name
         self._first_name = first_name
+        self._gender = gender
 
     @property
     def family_name(self):
@@ -31,3 +38,16 @@ class Person:
         氏名
         """
         return self._family_name + self._first_name
+
+    @property
+    def gender(self):
+        """
+        性別
+        """
+        return self._gender
+
+    def is_male(self):
+        return self.gender == MALE
+
+    def is_female(self):
+        return self.gender == FEMALE
--- tests/acme/test_person.py
+++ tests/acme/test_person.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 from wsgiref.validate import assert_
 import pytest
-from acme.person import Person
+from acme.person import Person, MALE, FEMALE
 
 
 class Test佐藤一郎:
-    佐藤一郎 = Person('佐藤', '一郎')
+    佐藤一郎 = Person('佐藤', '一郎', MALE)
 
     def test_佐藤一郎はPersonである(self):
         assert type(self.佐藤一郎) is Person
@@ -20,8 +20,16 @@ class Test佐藤一郎:
     def test_佐藤一郎の氏名は佐藤一郎である(self):
         assert self.佐藤一郎.full_name == '佐藤一郎'
 
+    def test_佐藤一郎は男である(self):
+        assert self.佐藤一郎.gender is MALE
+        assert self.佐藤一郎.is_male() == True
+
+    def test_佐藤一郎は女ではない(self):
+        assert self.佐藤一郎.gender is not FEMALE
+        assert self.佐藤一郎.is_female() == False
+
 class Test鈴木花子:
-    鈴木花子 = Person('鈴木', '花子')
+    鈴木花子 = Person('鈴木', '花子', FEMALE)
 
     def test_鈴木花子の名字は鈴木である(self):
         assert self.鈴木花子.family_name == '鈴木'
@@ -32,17 +40,27 @@ class Test鈴木花子:
     def test_鈴木花子の氏名は鈴木花子である(self):
         assert self.鈴木花子.full_name == '鈴木花子'
 
+    def test_鈴木花子は女である(self):
+        assert self.鈴木花子.gender is FEMALE
+        assert self.鈴木花子.is_female() == True
+
+    def test_鈴木花子は男ではない(self):
+        assert self.鈴木花子.gender is not MALE
+        assert self.鈴木花子.is_male() == False
+
 class TestPerson生成エラー:
-    @pytest.mark.parametrize(("family_name", "first_name"),[
-        ("佐藤", ""),
-        ("", "一郎"),
-        ("", ""),
-        ("佐藤", None),
-        (None, "一郎"),
-        (None, None),
-        ("佐藤", " "),
-        ("  ", "一郎"),
+    @pytest.mark.parametrize(("family_name", "first_name", "gender"),[
+        ("佐藤", "", MALE),
+        ("", "一郎", MALE),
+        ("", "", None),
+        ("佐藤", None, MALE),
+        (None, "一郎", MALE),
+        (None, None, None),
+        ("佐藤", " ", MALE),
+        ("  ", "一郎", MALE),
+        ("佐藤", "一郎", None),
+        ("佐藤", "一郎", 3),
     ])
-    def test_Person生成エラー(self, family_name, first_name):
+    def test_Person生成エラー(self, family_name, first_name, gender):
         with pytest.raises(RuntimeError):
-            Person(family_name, first_name)
+            Person(family_name, first_name, gender)
