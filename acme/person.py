eb8e22fb4fd8be949d81f27cc276a538535db62a
Satoru Motegi <mokky.monkey.14@gmail.com>
2014-10-16 21:33:32 +0900

    課題１実装

--- acme/person.py
+++ acme/person.py
@@ -3,18 +3,31 @@
 
 class Person:
     def __init__(self, family_name, first_name):
+        if family_name is None or family_name == "":
+            raise RuntimeError("family_name must required.")
+        elif first_name is None or first_name == "":
+            raise RuntimeError("first_name must required.")
+
         self._family_name = family_name
         self._first_name = first_name
 
     @property
     def family_name(self):
+        """
+        苗字
+        """
         return self._family_name
 
     @property
     def first_name(self):
+        """
+        名前
+        """
         return self._first_name
 
     @property
     def full_name(self):
+        """
+        氏名
+        """
         return self._family_name + self._first_name
-
--- tests/acme/test_person.py
+++ tests/acme/test_person.py
@@ -1,18 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import pytest
 from acme.person import Person
 
 
-class TestPerson:
-    def test_佐藤一郎の名字は佐藤である(self):
-        佐藤一郎 = Person('佐藤', '一郎')
-        assert 佐藤一郎.family_name == '佐藤'
+class Test佐藤一郎:
+    佐藤一郎 = Person('佐藤', '一郎')
+
+    def test_佐藤一郎はPersonである(self):
+        assert type(self.佐藤一郎) is Person
+
+    def test_佐藤一郎の苗字は佐藤である(self):
+        assert self.佐藤一郎.family_name == '佐藤'
 
     def test_佐藤一郎の名前は一郎である(self):
-        佐藤一郎 = Person('佐藤', '一郎')
-        assert 佐藤一郎.first_name == '一郎'
+        assert self.佐藤一郎.first_name == '一郎'
 
     def test_佐藤一郎の氏名は佐藤一郎である(self):
-        佐藤一郎 = Person('佐藤', '一郎')
-        assert 佐藤一郎.full_name == '佐藤一郎'
+        assert self.佐藤一郎.full_name == '佐藤一郎'
 
+
+@pytest.mark.parametrize(("family_name", "first_name"),[
+    ("佐藤", ""),
+    ("", "一郎"),
+    ("", ""),
+    ("佐藤", None),
+    (None, "一郎"),
+    (None, None),
+])
+def test_Person生成エラー(family_name, first_name):
+    with pytest.raises(RuntimeError):
+        Person(family_name, first_name)
