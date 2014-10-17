cd6b69097ca5afb63de13facd480d7af1cd264b7
Satoru Motegi <mokky.monkey.14@gmail.com>
2014-10-18 02:30:28 +0900

    誕生日を追加

--- acme/person.py
+++ acme/person.py
@@ -1,22 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from datetime import date
 
 MALE = 1
 FEMALE = 2
 GENDER = (MALE, FEMALE)
 
 class Person:
-    def __init__(self, family_name, first_name, gender):
+    def __init__(self, family_name, first_name, gender, birthday = date.today()):
         if not family_name or family_name.strip() == "":
             raise RuntimeError("family_name must required.")
-        elif not first_name or first_name.strip() == "":
+        if not first_name or first_name.strip() == "":
             raise RuntimeError("first_name must required.")
-        elif gender not in GENDER:
+        if gender not in GENDER:
             raise RuntimeError("illegal gender.")
+        if type(birthday) is not date or birthday > date.today():
+            raise RuntimeError("illegal birthday.")
 
         self._family_name = family_name
         self._first_name = first_name
         self._gender = gender
+        self._birthday = birthday
 
     @property
     def family_name(self):
@@ -45,6 +49,13 @@ class Person:
         性別
         """
         return self._gender
+
+    @property
+    def birthday(self):
+        """
+        誕生日
+        """
+        return self._birthday
 
     def is_male(self):
         return self.gender is MALE
--- tests/acme/test_person.py
+++ tests/acme/test_person.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from datetime import date, timedelta
 from wsgiref.validate import assert_
 import pytest
 from acme.person import Person, MALE, FEMALE
 
 
 class Test佐藤一郎:
-    佐藤一郎 = Person('佐藤', '一郎', MALE)
+    birthday = date(1965, 10, 10)
+    佐藤一郎 = Person('佐藤', '一郎', MALE, birthday)
 
     def test_佐藤一郎はPersonである(self):
         assert type(self.佐藤一郎) is Person
@@ -27,6 +29,9 @@ class Test佐藤一郎:
     def test_佐藤一郎は女ではない(self):
         assert self.佐藤一郎.gender is not FEMALE
         assert self.佐藤一郎.is_female() == False
+
+    def test_佐藤一郎の誕生日が取得できること(self):
+        assert self.佐藤一郎.birthday == self.birthday
 
 class Test鈴木花子:
     鈴木花子 = Person('鈴木', '花子', FEMALE)
@@ -80,3 +85,12 @@ class Test不正パラメータ:
     def test_不正パラメータでRuntimeErrorとなること(self, family_name, first_name, gender):
         with pytest.raises(RuntimeError):
             Person(family_name, first_name, gender)
+
+    @pytest.mark.parametrize(("birthday"), [
+        (None),
+        ("2014/10/17"), # not date
+        (date.today() + timedelta(days=1)), # future
+    ])
+    def test_誕生日指定不正のときRuntimeErrorとなること(self, birthday):
+        with pytest.raises(RuntimeError):
+            Person("未来", "子", FEMALE, birthday)
